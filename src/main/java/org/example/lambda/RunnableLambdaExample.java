cb63d44c241a8b2d1fc1258db0812352bf5b294a
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-08-20 19:02:22 +0530

    implementing the runnable interface using the lambda function

--- src/main/java/org/example/lambda/RunnableLambdaExample.java
+++ src/main/java/org/example/lambda/RunnableLambdaExample.java
@@ -11,6 +11,14 @@ public class RunnableLambdaExample {
     public static void main(String[] args) {
         Thread thread = new Thread(new ThreadDemo());
         thread.start();
+
+        Runnable runnable = ()-> {
+            System.out.println("run method called using the lambda function");
+        };
+        Thread lambdaThread = new Thread(runnable);
+        lambdaThread.start();
+
+        Thread lambdaT1 = new Thread(()-> System.out.println("passing lambda in parameter"));
     }
 
 }
