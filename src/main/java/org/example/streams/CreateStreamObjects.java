b58755f00b2155f583c16e1dae15a337e3f63e0f
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-08-20 23:21:49 +0530

    added the creation and filter of Stream api's

--- src/main/java/org/example/streams/CreateStreamObjects.java
+++ src/main/java/org/example/streams/CreateStreamObjects.java
@@ -1,2 +1,38 @@
-package org.example.streams;public class CreateStreamObjects {
+package org.example.streams;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class CreateStreamObjects {
+    public static void main(String[] args) {
+        // creating a stream
+        Stream<String> stream = Stream.of("a", "b", "c");
+        stream.forEach(System.out::println);
+
+
+        // creating a Stream from collections
+        Collection<String> collection = Arrays.asList("java", "j2ee", "Spring","hibernate");
+        Stream<String> stream2 = collection.stream();
+        stream2.forEach(System.out::println);
+
+        List<String> list = Arrays.asList("java", "j2ee", "Spring","hibernate");
+        Stream<String> listStream = list.stream();
+        listStream.forEach(System.out::println);
+
+
+        Set<String> set = new HashSet<>(list);
+        Stream<String> setStream = set.stream();
+        setStream.forEach(System.out::println);
+
+        String[] strArray = {"a", "b", "c", "d", "e"};
+        Stream<String> strArrayStream = Arrays.stream(strArray );
+        strArrayStream.forEach(System.out::println);
+
+
+
+
+
+
+
+    }
 }
--- src/main/java/org/example/streams/StreamFilter.java
+++ src/main/java/org/example/streams/StreamFilter.java
@@ -1,2 +1,88 @@
-package org.example.streams;public class StreamFilter {
+package org.example.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilter {
+    private static List<Product> getProducts(){
+        List<Product> products = new ArrayList<Product>();
+        products.add(new Product(1, "1",1));
+        products.add(new Product(2, "2",2));
+        products.add(new Product(3, "3",3));
+        products.add(new Product(4, "4",4));
+        return products;
+    }
+    public static void main(String[] args) {
+
+        // without using streams to filter the products having the price > 2
+        List<Product> products = new ArrayList<>();
+        for(Product p :getProducts()) {
+            if(p.getPrice() > 2){
+                products.add(p);
+            }
+        }
+        for(Product p :products){
+            System.out.println(p);
+
+        }
+
+        System.out.println("***********************************");
+
+        // using stream to filter the producst having the price > 2
+
+        List<Product> filteredProducts = getProducts().stream().filter(product -> product.getPrice() > 2 ? true : false).collect(Collectors.toList());
+        filteredProducts.forEach(product -> System.out.println(product));
+
+
+
+
+
+    }
+}
+
+class Product{
+    private int id;
+    private String name;
+    private  float price;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public Product(int id, String name, float price) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
