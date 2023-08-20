7d1a6e74ba441a7d42bb20c317a4cb7b92877b7b
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-08-20 16:39:08 +0530

    initial commit differentiating with functional interfaces and lambda expressions

---
+++ src/main/java/org/example/lambda/LambdaExample.java
@@ -0,0 +1,46 @@
+package org.example.lambda;
+
+
+
+interface Shape{
+    void draw();
+}
+
+
+class Rectangle implements  Shape{
+    @Override
+    public void draw() {
+        System.out.println("Rectangle draw");
+    }
+}
+class Circle implements  Shape{
+    @Override
+    public void draw() {
+        System.out.println("circle draw");
+    }
+}
+class square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("square draw");
+    }
+}
+
+
+public class LambdaExample {
+    public static void main(String[] args) {
+
+        // with out using lambda functions
+        Shape rectangleOld = new Rectangle();
+        rectangleOld.draw();
+
+
+        // using lambda functions
+        Shape rectangle = () -> {
+            System.out.println("rectangle draw");
+        };
+        rectangle.draw();
+
+    }
+
+}
